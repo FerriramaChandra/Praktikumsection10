b09c2cbdfd5f64ce0ca14159a62eec6403b48bba
FerriRama Chandra <FerriRamaChandra@gmail.com>
2022-03-14 17:30:33 +0800

    Tugas section 10

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Home from './Home';
+import Contact_us from './Contact_us';
+
+
+function App() {
+  return (
+    <div>
+      <Home/>
+      <Contact_us/>
+    </div>
+    
+  );
+}
+
+export default App;
